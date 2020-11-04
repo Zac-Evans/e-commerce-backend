a12a28ae8554b493a08bab5d400d6ddf43374166
zac-evans <zacevanscoding@gmail.com>
2020-11-04 11:49:08 -0500

    removed config from gitignore

--- models/index.js
+++ models/index.js
@@ -5,7 +5,7 @@ const path = require("path");
 const Sequelize = require("sequelize");
 const basename = path.basename(__filename);
 const env = process.env.NODE_ENV || "development";
-const config = require("/../config/config");
+const config = require(__dirname + "/../config/config.json");
 const db = {};
 
 let sequelize;
@@ -14,9 +14,9 @@ if (false) {
   sequelize = new Sequelize(process.env[config.use_env_variable], config);
 } else {
   sequelize = new Sequelize(
-    config.database,
-    config.username,
-    config.password,
+    config.development.database,
+    config.development.username,
+    config.development.password,
     config
   );
 }
